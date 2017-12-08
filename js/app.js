1a82c68a6c718a20d6cfcf763a50c9426bb39711
rjmcbain <rmcbain01@gmail.com>
2017-12-07 17:59:48 -0700

    it moved

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 console.log("WTF IS GOING ON!!!???")
+
+$("button").click(function(){
+    $("#red").animate({left: '1000px'});
+}); 
+
+$("button").click(function(){
+    $("#blue").animate({left: '250px'});
+}); 
