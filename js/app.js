79fba525eadcaea7e803278ee911bcc4eba1608c
rjmcbain <rmcbain01@gmail.com>
2017-12-07 18:56:22 -0700

    nothing works

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 console.log("WTF IS GOING ON!!!???")
 
-$("button").click(function(){
+/*$("button").click(function(){
     $("#red").animate({left: '250px'});
 }); 
 
@@ -8,3 +8,23 @@ $("button").click(function(){
     $("#blue").animate({left: '1000px'});
 }); 
 
+$(window).keypress(function(){
+    console.log("yo");
+});*/
+/*$(document).keypress(function(e) {
+    if(e.which == 13) {
+        console.log("p1");
+    }
+});*/
+
+$(window).keypress(function(q) {
+    if(q.which == 49)
+    console.log("player1");
+ $("#red").animate({width: "+=100px"});
+});
+
+$(window).keypress(function(q) {
+    if(q.which == 50)
+    console.log("player2");
+ $("#blue").animate({width: "+=100px"});
+});
